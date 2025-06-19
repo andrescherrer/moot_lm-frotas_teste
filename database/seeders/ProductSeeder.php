361d8e2ea02eeaf42ac4586b018566fb1f895218
André C. Scherrer <andrescherrer@gmail.com>
2025-06-19 17:29:16 -0300

    feat: update ProductSeeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -25,22 +25,72 @@ class ProductSeeder extends Seeder
                 'categories' => ['Calçados', 'Esportes']
             ],
             [
+                'name' => 'Tênis Adidas Superstart',
+                'brand_id' => $brands->where('name', 'Adidas')->first()->id,
+                'categories' => ['Calçados', 'Esportes']
+            ],
+            [
                 'name' => 'Camiseta Dry-Fit',
                 'brand_id' => $brands->where('name', 'Adidas')->first()->id,
                 'categories' => ['Roupas', 'Esportes']
             ],
             [
-                'name' => 'iPhone 15',
+                'name' => 'Celular iPhone 15',
                 'brand_id' => $brands->where('name', 'Apple')->first()->id,
                 'categories' => ['Eletrônicos']
             ],
             [
-                'name' => 'Galaxy S23',
+                'name' => 'Celular Galaxy S23',
                 'brand_id' => $brands->where('name', 'Samsung')->first()->id,
                 'categories' => ['Eletrônicos', 'Acessórios']
             ],
             [
                 'name' => 'Fone WH-1000XM5',
+                'brand_id' => $brands->where('name', 'Sony')->first()->id,
+                'categories' => ['Eletrônicos', 'Acessórios']
+            ],            
+            [
+                'name' => 'Camiseta T-Shirt Quality',
+                'brand_id' => $brands->where('name', 'Nike')->first()->id,
+                'categories' => ['Roupas', 'Esportes']
+            ],
+            [
+                'name' => 'Celular iPhone 14',
+                'brand_id' => $brands->where('name', 'Apple')->first()->id,
+                'categories' => ['Eletrônicos']
+            ],
+            [
+                'name' => 'Celular Galaxy S22',
+                'brand_id' => $brands->where('name', 'Samsung')->first()->id,
+                'categories' => ['Eletrônicos', 'Acessórios']
+            ],
+            [
+                'name' => 'Fone CH520',
+                'brand_id' => $brands->where('name', 'Sony')->first()->id,
+                'categories' => ['Eletrônicos', 'Acessórios']
+            ],
+            [
+                'name' => 'Tênis Campus 00s Masculino',
+                'brand_id' => $brands->where('name', 'Adidas')->first()->id,
+                'categories' => ['Calçados']
+            ],
+            [
+                'name' => 'Camiseta Trefoil Essentials',
+                'brand_id' => $brands->where('name', 'Adidas')->first()->id,
+                'categories' => ['Roupas']
+            ],
+            [
+                'name' => 'Celular iPhone 13',
+                'brand_id' => $brands->where('name', 'Apple')->first()->id,
+                'categories' => ['Eletrônicos']
+            ],
+            [
+                'name' => 'Celular Galaxy S20',
+                'brand_id' => $brands->where('name', 'Samsung')->first()->id,
+                'categories' => ['Eletrônicos', 'Acessórios']
+            ],
+            [
+                'name' => 'Fone INZONE H9',
                 'brand_id' => $brands->where('name', 'Sony')->first()->id,
                 'categories' => ['Eletrônicos', 'Acessórios']
             ],
