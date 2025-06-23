8892601ad502bd948b231616f9333367752be6f7
André C. Scherrer <andrescherrer@gmail.com>
2025-06-23 08:12:27 -0300

    fix: adjust docker files and styles blade files

--- docker/php/startup.sh
+++ docker/php/startup.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-set -e
-
-mkdir -p /var/www/storage/framework/{cache,sessions,views}
-mkdir -p /var/www/bootstrap/cache
-
-find /var/www/storage -type d -exec chmod 775 {} \; || true
-find /var/www/bootstrap/cache -type d -exec chmod 775 {} \; || true
-
-echo "foi"
-
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan optimize:clear
 exec "$@"
