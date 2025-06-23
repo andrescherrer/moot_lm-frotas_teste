f7f93d165cdf9d2af24af04dfb110e6ab20f7c0a
André C. Scherrer <andrescherrer@gmail.com>
2025-06-22 23:12:44 -0300

    fix: fix docker-compose.yml, docker/php/Dockerfile and docker/php/startup.sh

---
+++ docker/php/startup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+mkdir -p /var/www/storage/framework/{cache,sessions,views}
+mkdir -p /var/www/bootstrap/cache
+
+find /var/www/storage -type d -exec chmod 775 {} \; || true
+find /var/www/bootstrap/cache -type d -exec chmod 775 {} \; || true
+
+exec "$@"
