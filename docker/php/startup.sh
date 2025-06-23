f81ff9f7f5189eea1e68b2fbebf9f5a94ce02f2d
André C. Scherrer <andrescherrer@gmail.com>
2025-06-22 23:58:54 -0300

    fix: docker config

--- docker/php/startup.sh
+++ docker/php/startup.sh
@@ -7,4 +7,6 @@ mkdir -p /var/www/bootstrap/cache
 find /var/www/storage -type d -exec chmod 775 {} \; || true
 find /var/www/bootstrap/cache -type d -exec chmod 775 {} \; || true
 
+echo "foi"
+
 exec "$@"
